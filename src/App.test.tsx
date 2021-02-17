a22e843d4ea5078001df30ea824d3d6e0b8b52ee
Ryan <dsouza.ryan.11@gmail.com>
2021-02-17 12:39:51 +0530

    fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 it('renders the first 20 countries', async () => {
-  render(<App />)
+  renderWithQueryClient(<App />)
 
   // before fetching
   const loadingText = screen.getByText(/loading/i)
   expect(document.body.contains(loadingText))
 })
+
+const renderWithQueryClient = (children: JSX.Element) => {
+  const client = new QueryClient()
+  return render(
+    <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  )
+}
